839c7b28bb85ee9bf7bbfb7b587e381df6f34512
Justin Britt <britt@uwindsor.ca>
2023-06-21 11:42:46 -0400

    Implement IComparable

--- Ma2013.A.E.O/Classes/IndexElements/Common/aIndexElement.cs
+++ Ma2013.A.E.O/Classes/IndexElements/Common/aIndexElement.cs
@@ -24,5 +24,13 @@
 
         /// <inheritdoc/>
         public FhirDateTime Value { get; }
+
+        /// <inheritdoc/>
+        public int CompareTo(
+            IaIndexElement other)
+        {
+            return this.Key.CompareTo(
+                other.Key);
+        }
     }
 }
