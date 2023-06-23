39d626e9a6e2a09a1fb0d61cb30b46a1707acc8d
Justin Britt <britt@uwindsor.ca>
2023-06-23 11:30:13 -0400

    Added visitor

---
+++ Ma2013.A.E.O/InterfacesVisitors/Contexts/SP/IPatientGroupProfitsVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace Ma2013.A.E.O.InterfacesVisitors.Contexts.SP
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Ma2013.A.E.O.Interfaces.IndexElements.Common;
+    using Ma2013.A.E.O.Interfaces.ParameterElements.SP.PatientGroupProfits;
+
+    public interface IPatientGroupProfitsVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : INullableValue<int>
+        where TValue : Money
+    {
+        RedBlackTree<IpIndexElement, IrParameterElement> RedBlackTree { get; }
+    }
+}
---
+++ Ma2013.A.E.O/Visitors/Contexts/SP/PatientGroupProfitsVisitor.cs
@@ -0,0 +1,55 @@
+﻿namespace Ma2013.A.E.O.Visitors.Contexts.SP
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Ma2013.A.E.O.Interfaces.IndexElements.Common;
+    using Ma2013.A.E.O.Interfaces.Indices.Common;
+    using Ma2013.A.E.O.Interfaces.ParameterElements.SP.PatientGroupProfits;
+    using Ma2013.A.E.O.InterfacesFactories.ParameterElements.SP.PatientGroupProfits;
+    using Ma2013.A.E.O.InterfacesVisitors.Contexts.SP;
+
+    internal sealed class PatientGroupProfitsVisitor<TKey, TValue> : IPatientGroupProfitsVisitor<TKey, TValue>
+        where TKey : INullableValue<int>
+        where TValue : Money
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public PatientGroupProfitsVisitor(
+            IrParameterElementFactory rParameterElementFactory,
+            Ip p)
+        {
+            this.rParameterElementFactory = rParameterElementFactory;
+
+            this.p = p;
+
+            this.RedBlackTree = new RedBlackTree<IpIndexElement, IrParameterElement>();
+        }
+
+        private IrParameterElementFactory rParameterElementFactory { get; }
+
+        private Ip p { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IpIndexElement, IrParameterElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IpIndexElement pIndexElement = this.p.GetElementAt(
+                obj.Key);
+
+            this.RedBlackTree.Add(
+                pIndexElement,
+                this.rParameterElementFactory.Create(
+                    pIndexElement,
+                    obj.Value));
+        }
+    }
+}
