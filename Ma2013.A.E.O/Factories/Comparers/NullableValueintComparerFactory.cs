1ccb6f0417416f32acd2723d16f34ec9b6e65c0b
Justin Britt <britt@uwindsor.ca>
2023-06-23 09:43:02 -0400

    Added factory

---
+++ Ma2013.A.E.O/Factories/Comparers/NullableValueintComparerFactory.cs
@@ -0,0 +1,28 @@
+﻿namespace Ma2013.A.E.O.Factories.Comparers
+{
+    using Ma2013.A.E.O.Classes.Comparers;
+    using Ma2013.A.E.O.Interfaces.Comparers;
+    using Ma2013.A.E.O.InterfacesFactories.Comparers;
+
+    internal sealed class NullableValueintComparerFactory : INullableValueintComparerFactory
+    {
+        public NullableValueintComparerFactory()
+        {
+        }
+
+        public INullableValueintComparer Create()
+        {
+            INullableValueintComparer instance = null;
+
+            try
+            {
+                instance = new NullableValueintComparer();
+            }
+            finally
+            {
+            }
+
+            return instance;
+        }
+    }
+}
---
+++ Ma2013.A.E.O/InterfacesFactories/Comparers/INullableValueintComparerFactory.cs
@@ -0,0 +1,9 @@
+﻿namespace Ma2013.A.E.O.InterfacesFactories.Comparers
+{
+    using Ma2013.A.E.O.Interfaces.Comparers;
+
+    public interface INullableValueintComparerFactory
+    {
+        INullableValueintComparer Create();
+    }
+}
