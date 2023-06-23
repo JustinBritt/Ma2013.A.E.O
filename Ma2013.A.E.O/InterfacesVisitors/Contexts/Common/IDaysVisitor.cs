609e3cd1086a7888dea82f59da01c56acd399402
Justin Britt <britt@uwindsor.ca>
2023-06-23 13:50:57 -0400

    Added visitor

---
+++ Ma2013.A.E.O/InterfacesVisitors/Contexts/Common/IDaysVisitor.cs
@@ -0,0 +1,18 @@
+﻿namespace Ma2013.A.E.O.InterfacesVisitors.Contexts.Common
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Ma2013.A.E.O.Interfaces.IndexElements.Common;
+
+    public interface IDaysVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : INullableValue<int>
+        where TValue : FhirDateTime
+    {
+        RedBlackTree<FhirDateTime, IdIndexElement> RedBlackTree { get; }
+    }
+}
---
+++ Ma2013.A.E.O/Visitors/Contexts/Common/DaysVisitor.cs
@@ -0,0 +1,48 @@
+﻿namespace Ma2013.A.E.O.Visitors.Contexts.Common
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Ma2013.A.E.O.Interfaces.Comparers;
+    using Ma2013.A.E.O.Interfaces.IndexElements.Common;
+    using Ma2013.A.E.O.InterfacesFactories.IndexElements.Common;
+    using Ma2013.A.E.O.InterfacesVisitors.Contexts.Common;
+
+    internal sealed class DaysVisitor<TKey, TValue> : IDaysVisitor<TKey, TValue>
+        where TKey : INullableValue<int>
+        where TValue : FhirDateTime
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public DaysVisitor(
+            IdIndexElementFactory dIndexElementFactory,
+            IFhirDateTimeComparer FhirDateTimeComparer)
+        {
+            this.dIndexElementFactory = dIndexElementFactory;
+
+            this.RedBlackTree = new RedBlackTree<FhirDateTime, IdIndexElement>(
+                FhirDateTimeComparer);
+        }
+
+        private IdIndexElementFactory dIndexElementFactory { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<FhirDateTime, IdIndexElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            this.RedBlackTree.Add(
+                obj.Value,
+                this.dIndexElementFactory.Create(
+                    obj.Key.Value.Value,
+                    obj.Value));
+        }
+    }
+}
