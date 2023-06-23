a78d3061767202f300681e0400120cba6eb7a56a
Justin Britt <britt@uwindsor.ca>
2023-06-23 12:41:15 -0400

    Added visitor

---
+++ Ma2013.A.E.O/InterfacesVisitors/Contexts/TP/IWardγVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace Ma2013.A.E.O.InterfacesVisitors.Contexts.TP
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Ma2013.A.E.O.Interfaces.IndexElements.Common;
+    using Ma2013.A.E.O.Interfaces.ParameterElements.TP.WardObjectiveFunctionWeights;
+    
+    public interface IWardγVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : Organization
+        where TValue : INullableValue<decimal>
+    {
+        RedBlackTree<IwIndexElement, IγParameterElement> RedBlackTree { get; }
+    }
+}
---
+++ Ma2013.A.E.O/Visitors/Contexts/TP/WardγVisitor.cs
@@ -0,0 +1,55 @@
+﻿namespace Ma2013.A.E.O.Visitors.Contexts.TP
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Ma2013.A.E.O.Interfaces.IndexElements.Common;
+    using Ma2013.A.E.O.Interfaces.Indices.Common;
+    using Ma2013.A.E.O.Interfaces.ParameterElements.TP.WardObjectiveFunctionWeights;
+    using Ma2013.A.E.O.InterfacesFactories.ParameterElements.TP.WardObjectiveFunctionWeights;
+    using Ma2013.A.E.O.InterfacesVisitors.Contexts.TP;
+
+    internal sealed class WardγVisitor<TKey, TValue> : IWardγVisitor<TKey, TValue>
+        where TKey : Organization
+        where TValue : INullableValue<decimal>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public WardγVisitor(
+            IγParameterElementFactory γParameterElementFactory,
+            Iw w)
+        {
+            this.γParameterElementFactory = γParameterElementFactory;
+
+            this.w = w;
+
+            this.RedBlackTree = new RedBlackTree<IwIndexElement, IγParameterElement>();
+        }
+
+        private IγParameterElementFactory γParameterElementFactory { get; }
+
+        private Iw w { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IwIndexElement, IγParameterElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IwIndexElement wIndexElement = this.w.GetElementAt(
+                obj.Key);
+
+            this.RedBlackTree.Add(
+                wIndexElement,
+                this.γParameterElementFactory.Create(
+                    wIndexElement,
+                    obj.Value));
+        }
+    }
+}
