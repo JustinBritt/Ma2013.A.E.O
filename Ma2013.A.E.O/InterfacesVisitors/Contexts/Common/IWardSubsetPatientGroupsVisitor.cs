4d70b1cd9fbef7723e7bf3aadda761943bdc2ef9
Justin Britt <britt@uwindsor.ca>
2023-06-23 14:25:25 -0400

    Added visitor

---
+++ Ma2013.A.E.O/InterfacesVisitors/Contexts/Common/IWardSubsetPatientGroupsVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace Ma2013.A.E.O.InterfacesVisitors.Contexts.Common
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Ma2013.A.E.O.Interfaces.IndexElements.Common;
+    using Ma2013.A.E.O.Interfaces.ParameterElements.Common.WardSubsetPatientGroups;
+  
+    public interface IWardSubsetPatientGroupsVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : Organization
+        where TValue : INullableValue<int>
+    {
+        RedBlackTree<IwIndexElement, IPParameterElement> RedBlackTree { get; }
+    }
+}
---
+++ Ma2013.A.E.O/Visitors/Contexts/Common/WardSubsetPatientGroupsVisitor.cs
@@ -0,0 +1,63 @@
+﻿namespace Ma2013.A.E.O.Visitors.Contexts.Common
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Ma2013.A.E.O.Interfaces.IndexElements.Common;
+    using Ma2013.A.E.O.Interfaces.Indices.Common;
+    using Ma2013.A.E.O.Interfaces.ParameterElements.Common.WardSubsetPatientGroups;
+    using Ma2013.A.E.O.InterfacesFactories.ParameterElements.Common.WardSubsetPatientGroups;
+    using Ma2013.A.E.O.InterfacesVisitors.Contexts.Common;
+
+    internal sealed class WardSubsetPatientGroupsVisitor<TKey, TValue> : IWardSubsetPatientGroupsVisitor<TKey, TValue>
+        where TKey : Organization
+        where TValue : INullableValue<int>
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public WardSubsetPatientGroupsVisitor(
+            IPParameterElementFactory PParameterElementFactory,
+            Ip p,
+            Iw w)
+        {
+            this.PParameterElementFactory = PParameterElementFactory;
+
+            this.p = p;
+
+            this.w = w;
+
+            this.RedBlackTree = new RedBlackTree<IwIndexElement, IPParameterElement>();
+        }
+
+        private IPParameterElementFactory PParameterElementFactory { get; }
+
+        private Ip p { get; }
+
+        private Iw w { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IwIndexElement, IPParameterElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IwIndexElement wIndexElement = this.w.GetElementAt(
+                obj.Key);
+
+            IpIndexElement pIndexElement = this.p.GetElementAt(
+                obj.Value);
+
+            this.RedBlackTree.Add(
+                wIndexElement,
+                this.PParameterElementFactory.Create(
+                    wIndexElement,
+                    pIndexElement));
+        }
+    }
+}
