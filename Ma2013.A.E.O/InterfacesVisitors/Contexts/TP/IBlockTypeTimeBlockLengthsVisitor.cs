9db08811e17dabc8c87a6652d6760aab7a8b9ae6
Justin Britt <britt@uwindsor.ca>
2023-06-23 14:47:33 -0400

    Added visitor

---
+++ Ma2013.A.E.O/InterfacesVisitors/Contexts/TP/IBlockTypeTimeBlockLengthsVisitor.cs
@@ -0,0 +1,19 @@
+﻿namespace Ma2013.A.E.O.InterfacesVisitors.Contexts.TP
+{
+    using System.Collections.Generic;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+    using NGenerics.Patterns.Visitor;
+
+    using Ma2013.A.E.O.Interfaces.IndexElements.TP;
+    using Ma2013.A.E.O.Interfaces.ParameterElements.TP.BlockTypeTimeBlockLengths;
+
+    public interface IBlockTypeTimeBlockLengthsVisitor<TKey, TValue> : IVisitor<KeyValuePair<TKey, TValue>>
+        where TKey : INullableValue<int>
+        where TValue : Duration
+    {
+        RedBlackTree<IkIndexElement, ILengthParameterElement> RedBlackTree { get; }
+    }
+}
---
+++ Ma2013.A.E.O/Visitors/Contexts/TP/BlockTypeTimeBlockLengthsVisitor.cs
@@ -0,0 +1,55 @@
+﻿namespace Ma2013.A.E.O.Visitors.Contexts.TP
+{
+    using System.Collections.Generic;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using NGenerics.DataStructures.Trees;
+
+    using Ma2013.A.E.O.Interfaces.IndexElements.TP;
+    using Ma2013.A.E.O.Interfaces.Indices.TP;
+    using Ma2013.A.E.O.Interfaces.ParameterElements.TP.BlockTypeTimeBlockLengths;
+    using Ma2013.A.E.O.InterfacesFactories.ParameterElements.TP.BlockTypeTimeBlockLengths;
+    using Ma2013.A.E.O.InterfacesVisitors.Contexts.TP;
+
+    internal sealed class BlockTypeTimeBlockLengthsVisitor<TKey, TValue> : IBlockTypeTimeBlockLengthsVisitor<TKey, TValue>
+        where TKey : INullableValue<int>
+        where TValue : Duration
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public BlockTypeTimeBlockLengthsVisitor(
+            ILengthParameterElementFactory lengthParameterElementFactory,
+            Ik k)
+        {
+            this.LengthParameterElementFactory = lengthParameterElementFactory;
+
+            this.k = k;
+
+            this.RedBlackTree = new RedBlackTree<IkIndexElement, ILengthParameterElement>();
+        }
+
+        private ILengthParameterElementFactory LengthParameterElementFactory { get; }
+
+        private Ik k { get; }
+
+        public bool HasCompleted => false;
+
+        public RedBlackTree<IkIndexElement, ILengthParameterElement> RedBlackTree { get; }
+
+        public void Visit(
+            KeyValuePair<TKey, TValue> obj)
+        {
+            IkIndexElement kIndexElement = this.k.GetElementAt(
+                obj.Key);
+
+            this.RedBlackTree.Add(
+                kIndexElement,
+                this.LengthParameterElementFactory.Create(
+                    kIndexElement,
+                    obj.Value));
+        }
+    }
+}
