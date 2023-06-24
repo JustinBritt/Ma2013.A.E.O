a236b752e1c705d8ca0dbab315e84ede8f24aa6c
Justin Britt <britt@uwindsor.ca>
2023-06-24 16:35:10 -0400

    Added class

---
+++ Ma2013.A.E.O/Classes/Comparers/LocationComparer.cs
@@ -0,0 +1,28 @@
+﻿namespace Ma2013.A.E.O.Classes.Comparers
+{
+    using System;
+
+    using log4net;
+
+    using Hl7.Fhir.Model;
+
+    using Ma2013.A.E.O.Interfaces.Comparers;
+
+    internal sealed class LocationComparer : ILocationComparer
+    {
+        private ILog Log => LogManager.GetLogger(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType);
+
+        public LocationComparer()
+        {
+        }
+
+        public int Compare(
+            Location x,
+            Location y)
+        {
+            return String.CompareOrdinal(
+                x.Id,
+                y.Id);
+        }
+    }
+}
