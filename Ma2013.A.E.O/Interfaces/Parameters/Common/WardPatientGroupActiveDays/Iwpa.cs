99f55eeafe9f44b0ea1b3eb1e7762b336783cb14
Justin Britt <britt@uwindsor.ca>
2023-07-09 11:03:35 -0400

    Added property

--- Ma2013.A.E.O/Interfaces/Parameters/Common/WardPatientGroupActiveDays/Iwpa.cs
+++ Ma2013.A.E.O/Interfaces/Parameters/Common/WardPatientGroupActiveDays/Iwpa.cs
@@ -1,6 +1,12 @@
 ﻿namespace Ma2013.A.E.O.Interfaces.Parameters.Common.WardPatientGroupActiveDays
 {
+    using NGenerics.DataStructures.Trees;
+
+    using Ma2013.A.E.O.Interfaces.CrossJoinElements.Common;
+    using Ma2013.A.E.O.Interfaces.IndexElements.Common;
+
     public interface Iwpa
     {
+        RedBlackTree<IwIndexElement, RedBlackTree<IpIndexElement, RedBlackTree<IaIndexElement, IpaCrossJoinElement>>> Value { get; }
     }
 }
